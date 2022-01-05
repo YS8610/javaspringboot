52a85375fa3b84b65a563f35cb1cb9f4142b6188
YS_workPC <ong_ys86@yahoo.com.sg>
2022-01-05 14:26:01 +0800

    initial commit

---
+++ src/main/java/sg/nus/nus/ws11/Ws11Application.java
@@ -0,0 +1,13 @@
+package sg.nus.nus.ws11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws11Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/nus/ws11/Ws11ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.nus.ws11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
